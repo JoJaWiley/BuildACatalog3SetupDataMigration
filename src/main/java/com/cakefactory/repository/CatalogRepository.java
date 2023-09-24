9628bdcc8bbbafed19dce05fad76bbb93191908e
JoJaWiley <joshua.wiley@summitoic.edu>
2023-09-24 14:50:28 -0500

    JPAUnitTest complete & passing

--- src/main/java/com/cakefactory/repository/CatalogRepository.java
+++ src/main/java/com/cakefactory/repository/CatalogRepository.java
@@ -3,11 +3,11 @@ package com.cakefactory.repository;
 import com.cakefactory.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CatalogRepository extends JpaRepository<Item, String> {
 
-    @Query("SELECT * FROM items")
-    List<Item> findAllItems();
 }
--- src/test/java/com/cakefactory/JPAUnitTest.java
+++ src/test/java/com/cakefactory/JPAUnitTest.java
@@ -1,9 +1,16 @@
 package com.cakefactory;
 
+import com.cakefactory.entity.Item;
 import com.cakefactory.repository.CatalogRepository;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @DataJpaTest
 public class JPAUnitTest {
@@ -13,4 +20,17 @@ public class JPAUnitTest {
 
     @Autowired
     CatalogRepository repository;
+
+    @Test
+    public void findAllTest() {
+        Item item1 = new Item("a", "test item1", 2.99);
+        entityManager.persist(item1);
+
+        Item item2 = new Item("b", "test item2", 2.99);
+        entityManager.persist(item2);
+
+        List<Item> items = repository.findAll();
+
+        assertThat(items).hasSize(2).contains(item1, item2);
+    }
 }
